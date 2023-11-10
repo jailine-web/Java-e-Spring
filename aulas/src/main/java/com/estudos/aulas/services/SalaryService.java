90c18f9c1a936ca48b47be5e67753037a872872d
Jailine <jay.santana964@gmail.com>
2023-11-10 16:58:22 -0300

    Entendendo como o spring lida com injeção de dependência

---
+++ aulas/src/main/java/com/estudos/aulas/services/SalaryService.java
@@ -0,0 +1,23 @@
+package com.estudos.aulas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudos.aulas.entities.Employee;
+
+@Service
+public class SalaryService {
+
+	@Autowired
+	private TaxService taxService;
+	
+	@Autowired
+	private PensionService pensionService;
+
+	
+	public double netSalary(Employee employee) {
+		return employee.getGrossSalary() 
+				- taxService.tax(employee.getGrossSalary())
+				- pensionService.discount(employee.getGrossSalary()) ;
+	}
+}
