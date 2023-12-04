e4e3e8aee00124a56fb71e3f326c38550a924f40
Jailine <jay.santana964@gmail.com>
2023-12-04 15:37:41 -0300

    Consultas com query methods

--- aula-lazy/src/main/java/com/devsuperior/aulalazy/controllers/EmployeeController.java
+++ aula-lazy/src/main/java/com/devsuperior/aulalazy/controllers/EmployeeController.java
@@ -3,10 +3,12 @@ package com.devsuperior.aulalazy.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.aulalazy.dto.EmployeeDepartmentDTO;
@@ -20,10 +22,17 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService service;
 	
-	@GetMapping(value = "/{id}/min")
-	public ResponseEntity<EmployeeMinDTO> findByIdMin(@PathVariable Long id) {
-		EmployeeMinDTO obj = service.findByIdMin(id);		
-		return ResponseEntity.ok(obj);
+//	@GetMapping(value = "/{id}/min")
+//	public ResponseEntity<EmployeeMinDTO> findByIdMin(@PathVariable Long id) {
+//		EmployeeMinDTO obj = service.findByIdMin(id);		
+//		return ResponseEntity.ok(obj);
+//	}
+	
+	@GetMapping(value="/employee")
+	public ResponseEntity<List<EmployeeMinDTO>> findByName(
+			@RequestParam(name = "name", defaultValue = "") String name) {
+		List<EmployeeMinDTO> result = service.findByName(name);
+		return ResponseEntity.ok(result);
 	}
 
 	@GetMapping(value = "/{id}")
