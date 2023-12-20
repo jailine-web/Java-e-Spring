6f62f7b6d115098a645984505b507ece5923a808
Jailine <jay.santana964@gmail.com>
2023-12-19 21:02:40 -0300

    Resolução da 1° questão do desafio sem paginação

---
+++ desafio-consulta-vendas/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -0,0 +1,46 @@
+package com.devsuperior.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
+		SaleMinDTO dto = service.findById(id);
+		return ResponseEntity.ok(dto);
+	}
+
+	@GetMapping(value = "/report")
+	public ResponseEntity<List<SaleMinDTO>> getReport(
+			@RequestParam(value = "minDate"  , defaultValue = "") String dateMin, 
+			@RequestParam(value = "maxDate"  , defaultValue = "") String dateMax, 
+			@RequestParam(value = "name"     , defaultValue = "") String name) {
+		
+		List<SaleMinDTO> sales = service.findReport(dateMin, dateMax, name);
+		
+		return ResponseEntity.ok().body(sales);
+	}
+
+	@GetMapping(value = "/summary")
+	public ResponseEntity<SaleMinDTO> getSummary() {
+		// TODO
+		return null;
+	}
+}
---
+++ desafio-consulta-vendas/src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -0,0 +1,61 @@
+package com.devsuperior.dsmeta.dto;
+
+import java.time.LocalDate;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.projections.SaleProjection;
+
+public class SaleMinDTO {
+
+	private Long id;
+	private Double amount;
+	private LocalDate date; 
+	private String name;
+	
+	public SaleMinDTO(SaleProjection projection) {
+		id     = projection.getId();
+		amount = projection.getAmount();
+		date   = projection.getDate();
+		name   = projection.getName();
+	}
+	
+	public SaleMinDTO(Sale entity) {
+		id     = entity.getId();
+		amount = entity.getAmount();
+		date   = entity.getDate();
+	}
+
+	public SaleMinDTO(Long id, Double amount, LocalDate date, String name) {
+		this.id     = id;
+		this.amount = amount;
+		this.date   = date;
+		this.name   = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "SaleMinDTO [id=" + id + ", amount=" + amount + ", date=" + date + ", name=" + name + "]";
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	
+}
